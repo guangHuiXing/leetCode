70e9add00bc09247b3821c5e9e1526fec1c58ca7
guangHuiXing <2589490057@qq.com>
2020-04-26 12:57:31 +0800

    Initial commit

---
+++ src/com/xgh/ListNode.java
@@ -0,0 +1,15 @@
+package com.xgh;
+
+/**
+ * @author xgh
+ * @version 1.0
+ * @date 2020/4/26 12:48
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
---
+++ src/com/xgh/Simple.java
@@ -0,0 +1,168 @@
+package com.xgh;
+
+/**
+ * @author xgh
+ * @version 1.0
+ * @date 2020/4/24 20:44
+ */
+public class Simple {
+
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode(int x) { val = x; }
+     * }
+     * <p>
+     * 将两个升序链表合并为一个新的升序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+     * <p>
+     * 示例：
+     * <p>
+     * 输入：1->2->4, 1->3->4
+     * 输出：1->1->2->3->4->4
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/merge-two-sorted-lists
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    class Solution {
+        public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+            if (l1 == null) {
+                return l2;
+            }
+            if (l2 == null) {
+                return l1;
+            }
+            if (l1.val < l2.val) {
+                l1.next = mergeTwoLists(l1.next, l2);
+                return l1;
+            } else {
+                l2.next = mergeTwoLists(l1, l2.next);
+                return l2;
+            }
+        }
+    }
+
+    /**
+     * 给定一个排序数组，你需要在 原地 删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+     * <p>
+     * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+     * <p>
+     * <p>
+     * <p>
+     * 示例 1:
+     * <p>
+     * 给定数组 nums = [1,1,2],
+     * <p>
+     * 函数应该返回新的长度 2, 并且原数组 nums 的前两个元素被修改为 1, 2。
+     * <p>
+     * 你不需要考虑数组中超出新长度后面的元素。
+     */
+
+    public int removeDuplicates(int[] nums) {
+        int length = nums.length;
+        if (length <= 1) {
+            return length;
+        }
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[i] != nums[j]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+
+
+    /**
+     * 给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。
+     * <p>
+     * 不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
+     * <p>
+     * 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+     * 给定 nums = [3,2,2,3], val = 3,
+     * <p>
+     * 函数应该返回新的长度 2, 并且 nums 中的前两个元素均为 2。
+     * <p>
+     * 你不需要考虑数组中超出新长度后面的元素。
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/remove-element
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @param nums
+     * @param val
+     * @return
+     */
+    public static int removeElement(int[] nums, int val) {
+        int length = nums.length;
+        int j = 0;
+        for (int i = 0; i < length; i++) {
+            if (nums[i] != val) {
+                nums[j] = nums[i];
+                j++;
+            }
+
+        }
+        return j;
+    }
+
+
+    /**
+     * 给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。如果不存在，则返回  -1。
+     * <p>
+     * 示例 1:
+     * <p>
+     * 输入: haystack = "hello", needle = "llx"
+     * 输出: 2
+     * 对于本题而言，当 needle 是空字符串时我们应当返回 0 。这与C语言的 strstr() 以及 Java的 indexOf() 定义相符。
+     *
+     * @param haystack
+     * @param needle
+     * @return
+     */
+    public static int strStr(String haystack, String needle) {
+        if (needle.length() == 0) {
+            return 0;
+        }
+        if (haystack.length() == 0 || haystack.length() < needle.length()) {
+            return -1;
+        }
+        char[] haystackchars = haystack.toCharArray();
+        char[] needlechars = needle.toCharArray();
+        int i = 0;
+
+        int hl = haystackchars.length;
+        int nl = needlechars.length;
+
+        while (i < hl - nl + 1) {
+            int j = 0;
+            if (haystackchars[i] == needlechars[j]) {
+                int n = 1;
+                for (int m = i + 1; m < i + nl; m++, n++) {
+                    if (haystackchars[m] != needlechars[n]) {
+                        break;
+                    }
+                }
+                if (n == nl) {
+                    return i;
+                }
+            }
+            i++;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        /*int[] nums = {1, 2, 3, 4};
+        System.out.printf("length:" + removeElement(nums, 2));*/
+        String s1 = "hello";
+        String s2 = "ll";
+        System.out.printf("result :%d", strStr(s1, s2));
+
+    }
+
+}
