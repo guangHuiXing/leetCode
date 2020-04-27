f6d2799b7ee064a17d86af5cbb54c446bd6c515a
guangHuiXing <2589490057@qq.com>
2020-04-27 09:39:35 +0800

    搜索插入位置

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # leetCode
 leetCode
 面试笔刷算法题。先从simple 开始。每日一题。
+
+
+知识点：
+排序必须用二分法.
--- src/com/xgh/Simple.java
+++ src/com/xgh/Simple.java
@@ -156,6 +156,70 @@ public class Simple {
         return -1;
     }
 
+
+    /**
+     * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+     * <p>
+     * 你可以假设数组中无重复元素。
+     * <p>
+     * 示例 1:
+     * <p>
+     * 输入: [1,3,5,6], 5
+     * 输出: 2
+     * 示例 2:
+     * <p>
+     * 输入: [1,3,5,6], 2
+     * 输出: 1
+     * 示例 3:
+     * <p>
+     * 输入: [1,3,5,6], 7
+     * 输出: 4
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/search-insert-position
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int searchInsert1(int[] nums, int target) {
+        int len = nums.length;
+        if(len == 0){
+            return 1;
+        }
+        int i ;
+        for (i = 0; i < len; i++) {
+            if (nums[i] >= target) {
+                break;
+            }
+        }
+        return i;
+    }
+
+    /**
+     * 二分算法
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int searchInsert2(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            if(nums[mid] == target) {
+                return mid;
+            } else if(nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+
+    }
+
+
     public static void main(String[] args) {
         /*int[] nums = {1, 2, 3, 4};
         System.out.printf("length:" + removeElement(nums, 2));*/
