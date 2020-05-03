71168b0c4492f21991a867b5c4edfb16ec06f66f
guangHuiXing <2589490057@qq.com>
2020-05-03 11:12:40 +0800

    二分法求平方根

--- src/com/xgh/Simple.java
+++ src/com/xgh/Simple.java
@@ -448,6 +448,49 @@ public class Simple {
 
     }
 
+
+    /**
+     * 实现 int sqrt(int x) 函数。
+     * <p>
+     * 计算并返回 x 的平方根，其中 x 是非负整数。
+     * <p>
+     * 由于返回类型是整数，结果只保留整数的部分，小数部分将被舍去。
+     * <p>
+     * 示例 1:
+     * <p>
+     * 输入: 4
+     * 输出: 2
+     * 示例 2:
+     * <p>
+     * 输入: 8
+     * 输出: 2
+     * 说明: 8 的平方根是 2.82842...,
+     *      由于返回类型是整数，小数部分将被舍去。
+     *
+     * @param x
+     * @return
+     */
+    public static int mySqrt(int x) {
+
+        // 为了照顾到 0 把左边界设置为 0
+        long left = 0;
+        // # 为了照顾到 1 把右边界设置为 x // 2 + 1
+        long right = x / 2 + 1;
+        while (left < right) {
+            // 注意：这里一定取右中位数，如果取左中位数，代码会进入死循环
+            // long mid = left + (right - left + 1) / 2;
+            long mid = (left + right + 1) >>> 1;
+            long square = mid * mid;
+            if (square > x) {
+                right = mid - 1;
+            } else {
+                left = mid;
+            }
+        }
+        // 因为一定存在，因此无需后处理
+        return (int) left;
+  }
+
     public static void main(String[] args) {
         int[] nums = {9};
         /* System.out.printf("length:" + removeElement(nums, 2));*/
@@ -456,7 +499,13 @@ public class Simple {
         System.out.printf("result :%d", strStr(s1, s2));*/
         //System.out.printf("" + countAndSay(5));
         //maxSubArray(nums);
-        System.out.println(plusOne(nums));
+        //System.out.println(plusOne(nums));
+        System.out.println(mySqrt(2147395600));
+        System.out.println(289398L * 289398L);
+        System.out.println(2147395600);
+        System.out.println(289399 * 289399);
+        System.out.println(46340 * 46340);
+        System.out.println(46350 * 46350);
     }
 
 }
