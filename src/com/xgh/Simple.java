8b12ed535a6422644993fa6374860477b2569b6c
guangHuiXing <2589490057@qq.com>
2020-05-07 09:49:01 +0800

    树状结构相同的树采用递归

--- src/com/xgh/Simple.java
+++ src/com/xgh/Simple.java
@@ -631,6 +631,57 @@ public class Simple {
 
     }
 
+
+    /**
+     * 给定两个二叉树，编写一个函数来检验它们是否相同。
+     *
+     * 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+     *
+     * 示例 1:
+     *
+     * 输入:       1         1
+     *           / \       / \
+     *          2   3     2   3
+     *
+     *         [1,2,3],   [1,2,3]
+     *
+     * 输出: true
+     * 示例 2:
+     *
+     * 输入:      1          1
+     *           /           \
+     *          2             2
+     *
+     *         [1,2],     [1,null,2]
+     *
+     * 输出: false
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/same-tree
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     * @param p
+     * @param q
+     * @return
+     */
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        /**
+         * 最简单的策略是使用递归。首先判断 p 和 q 是不是 None，然后判断它们的值是否相等。
+         * 若以上判断通过，则递归对子结点做同样操作。
+         */
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        if(p.val!= q.val){
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+
+
+    }
+
     public static void main(String[] args) {
         int[] nums = {9};
         /* System.out.printf("length:" + removeElement(nums, 2));*/
---
+++ src/com/xgh/TreeNode.java
@@ -0,0 +1,18 @@
+package com.xgh;
+
+/**
+ * @ClassName TreeNode
+ * @Description
+ * @Author Administrator
+ * @Date 2020/5/7 9:35
+ * @Version 1.0
+ **/
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
