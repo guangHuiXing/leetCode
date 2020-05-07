66363bb64f6e401b5e55baba1b4d63dc54af47a9
guangHuiXing <2589490057@qq.com>
2020-05-07 10:13:30 +0800

    对称二叉树 递归就对了

--- src/com/xgh/Simple.java
+++ src/com/xgh/Simple.java
@@ -679,7 +679,54 @@ public class Simple {
         }
         return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 
+    }
 
+
+    /**
+     * 给定一个二叉树，检查它是否是镜像对称的。
+     *
+     *  
+     *
+     * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+     *
+     *     1
+     *    / \
+     *   2   2
+     *  / \ / \
+     * 3  4 4  3
+     *  
+     *
+     * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+     *
+     *     1
+     *    / \
+     *   2   2
+     *    \   \
+     *    3    3
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/symmetric-tree
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     * @param root
+     * @return
+     */
+    public boolean isSymmetric(TreeNode root) {
+
+       return isMorrior(root, root);
+
+    }
+
+    public boolean isMorrior(TreeNode root1,TreeNode root2){
+        if(root1 ==null && root2 == null){
+            return true;
+        }
+        if(root1 == null||root2 ==null){
+            return false;
+        }
+        if(root1.val !=root2.val){
+            return false;
+        }
+        return isMorrior(root1.left,root2.right)&&isMorrior(root2.left,root1.right);
     }
 
     public static void main(String[] args) {
